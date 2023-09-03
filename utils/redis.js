d2faaac29ca82a0e52284b7447a44b781824feed
Ifeanyichukwu Ifeanyichukwu <Ifeanyichukwuhillary2019@gmail.com>
2023-09-03 03:10:13 +0000

    Fix: fix linting errors

--- utils/redis.js
+++ utils/redis.js
@@ -1,36 +1,34 @@
-import {createClient, print} from 'redis';
-import {promisify} from 'util';
+import { createClient } from 'redis';
+import { promisify } from 'util';
 
+class RedisClient {
+  constructor() {
+    this.client = createClient();
+    this.client.on('error', (err) => {
+      console.log(err);
+    });
+    this.client.on('ready', () => { this.isalive = true; });
+  }
 
-class RedisClient{
-    constructor() {
-        this.client = createClient();
-        this.client.on('error', (err) => {
-            console.log(err);
-        });
-        this.client.on('ready', () => {this.isalive = true});
-    }
+  isAlive() {
+    return this.client.connected;
+  }
 
-    isAlive() {
-        return this.client.connected;
-    }
+  async get(key) {
+    const getAsync = promisify(this.client.get).bind(this.client);
+    const value = await getAsync(key);
+    return value;
+  }
 
-    async get(key) {
-        const getAsync = promisify(this.client.get).bind(this.client);
-        const value = await getAsync(key);
-        return value;
-    }
+  async set(key, value, duration) {
+    this.client.set(key, value);
+    this.client.expire(key, duration);
+  }
 
-    async set(key, value, duration) {
-        this.client.set(key, value);
-        this.client.expire(key, duration);
-    }
-
-    async del(key) {
-        this.client.del(key);
-    }
+  async del(key) {
+    this.client.del(key);
+  }
 }
-
 
 const redisClient = new RedisClient();
 module.exports = redisClient;
