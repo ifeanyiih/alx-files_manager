40e26eed9719968374066af1b842b41aa4ba32e6
Ifeanyichukwu Ifeanyichukwu <Ifeanyichukwuhillary2019@gmail.com>
2023-09-03 15:40:35 +0000

    Complete: task 2

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+import AppController from '../controllers/AppController.js';
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/status', AppController.getStatus);
+router.get('/stats', AppController.getStats);
+
+module.exports = router;
